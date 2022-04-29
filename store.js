166d6613190ed2289e2c85a920b58a207fdb424b
nicksbraga <sbraga22@students.rowan.edu>
2022-04-29 04:10:57 -0400

    javascript for store

--- store.js
+++ store.js
@@ -73,7 +73,7 @@ function addItemToCart(title, price, imageSrc) {
     var cartRowContents = `
         <div class="cart-item cart-column">
             <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
-            < span class="cart-item-title" > ${title}</span >
+            <span class="cart-item-title" > ${title}</span >
         </div >
         <span class="cart-price cart-column">${price}</span>
         <div class="cart-quantity cart-column">
@@ -82,7 +82,7 @@ function addItemToCart(title, price, imageSrc) {
         </div>`
     cartRow.innerHTML = cartRowContents
     cartItems.append(cartRow)
-    cartRow.getElementsByClassName('btn-danger')[0], addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
     cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
 }
 
